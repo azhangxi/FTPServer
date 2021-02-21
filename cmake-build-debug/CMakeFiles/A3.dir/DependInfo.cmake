056da65c36cf817db6b45293b12d22c51d76903d
azhangxi <71112793+azhangxi@users.noreply.github.com>
2021-02-20 21:28:27 -0800

    Add files via upload

---
+++ cmake-build-debug/CMakeFiles/A3.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/cygdrive/d/Study/2020 Fall/CPSC 317/A3/CSftp.c" "/cygdrive/d/Study/2020 Fall/CPSC 317/A3/cmake-build-debug/CMakeFiles/A3.dir/CSftp.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
